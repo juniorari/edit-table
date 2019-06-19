fb7a3e567d49cb6b41258b20d1552a14cb8539f1
Jose Ari Junior <joseari.jr@gmail.com>
2019-06-19 19:02:49 -0400

    Putting LICENSE file

--- edit-table.js
+++ edit-table.js
@@ -1,15 +1,15 @@
 /**
  *
- version : 1.0.0
+ version : 1.0.1
  =========================================================
  EditTable
  https://github.com/juniorari/EditTable
  Copyright (c) 2019 José Ari Junior
  =========================================================
 
- The MIT License (MIT)
+ MIT License
 
- Copyright (c) 2019 José Ari Junior
+ Copyright (c) 2019 Jose Ari Junior
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
@@ -18,16 +18,17 @@
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
  
  * A simple edit one cell in the table
  *
